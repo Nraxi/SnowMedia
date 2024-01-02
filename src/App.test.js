3dc4793688cc25319432493dd1709b1adc303a1b
nraxi <olle.code@hotmail.com>
2024-01-02 15:36:43 +0100

    test updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; // Use MemoryRouter for testing
 import App from './App';
 
 test('renders App without crashing', () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  expect(screen.getByText('SNOWBOARD')).toBeInTheDocument();
 });
