d5d07d8ca6e9b029317598e6ac6a72be3527cb5a
nraxi <olle.code@hotmail.com>
2024-01-02 14:33:23 +0100

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,6 @@
-import React from 'react';
 import { render } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom'; // Importera BrowserRouter
 import App from './App';
 
-// Mocka useLocation för att simulera ett objekt med pathname
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useLocation: () => ({ pathname: '/mocked-path' }),
-}));
-
 test('renders App without crashing', () => {
-  render(
-    <Router>
-      <App />
-    </Router>
-  );
+  render(<App />);
 });
